38943a8b025219084552bada20d34a0eb4fb23fc
burhan-aj <burhanuddin.a@thecareerlabs.com>
2022-11-04 00:18:58 +0530

    UI-8 User Contacts

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ChatScreen from "../screens/ChatScreen";
 import ChatsScreen from "../screens/ChatsScreen";
+import ContactsScreen from "../screens/ContactsScreen";
 import MainTabNavigator from "./MainTabNavigator";
 
 const Stack = createNativeStackNavigator();
@@ -17,6 +18,7 @@ const Navigator = () => {
           options={{ headerShown: false }}
         />
         <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="Contacts" component={ContactsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
