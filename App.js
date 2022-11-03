60b5c1cf3f2459153419f950a8ab7c196f916e11
burhan-aj <burhanuddin.a@thecareerlabs.com>
2022-11-03 14:39:59 +0530

    UI-3 Props

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ChatListItem from "./src/components/ChatListItem/Index";
 
+const chat = {
+  id: "1",
+  user: {
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
+    name: "Lukas",
+  },
+  lastMessage: {
+    text: "Oke",
+    createdAt: "07:30",
+  },
+};
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ChatListItem chat={chat} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
